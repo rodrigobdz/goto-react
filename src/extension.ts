66a7a07cee718a1a1af84fb1118b9f6ef9ad3bdc
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2018-06-10 02:24:03 +0200

    Extension: Ability to switch between component and container
    Switches to the component/container of the current container/component.
    Includes error handling.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,66 @@
 "use strict";
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * Opens the component/container of the currently active container/component in VSCode.
+ */
+function openCounterpart() {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    return vscode.window.showWarningMessage("There is no active editor.");
+  }
+
+  const CONTAINER = "container";
+  const COMPONENT = "component";
+
+  const normalizedRelativePath = path.normalize(
+    vscode.workspace.asRelativePath(editor.document.fileName)
+  );
+
+  const isComponent = normalizedRelativePath.includes(COMPONENT);
+  const isContainer = normalizedRelativePath.includes(CONTAINER);
+
+  if (isContainer) {
+    return openFile(normalizedRelativePath, CONTAINER, COMPONENT);
+  } else if (isComponent) {
+    return openFile(normalizedRelativePath, COMPONENT, CONTAINER);
+  }
+
+  vscode.window.showErrorMessage(
+    `Current file is neither a ${CONTAINER} nor a ${COMPONENT}.`
+  );
+}
+
+/**
+ * Helper function to show file if exists.
+ * @param filePath Path to the currently open file.
+ * @param type component/container.
+ * @param replacementType component/container.
+ */
+async function openFile(
+  filePath: string,
+  type: string,
+  replacementType: string
+) {
+  const searchPath = filePath.replace(type + "s", replacementType + "s");
+  let result = (await vscode.workspace.findFiles(searchPath, null, 1))![0];
+
+  if (!result) {
+    vscode.window.showErrorMessage(`No ${replacementType} found for ${type}.`);
+  }
+
+  vscode.window.showTextDocument(result);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World!");
-  });
+  let disposable = vscode.commands.registerCommand(
+    "extension.openCounterpart",
+    openCounterpart
+  );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
